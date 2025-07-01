da53cbc5b5e39e04ca2fc21d5b085e3dc521afe9
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-07-01 16:15:56 +0530

    upload files

---
+++ C#_Program example/P1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Program
+{
+    internal class P1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
