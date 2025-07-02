b50dbca8912d04feb6a2e2f6ea9194ee869b8063
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-07-02 19:12:19 +0530

    addd

--- Csharp_BD/Aditya_Dodiya_23SOECE11014/Q22.cs
+++ Csharp_BD/Aditya_Dodiya_23SOECE11014/Q22.cs
@@ -7,7 +7,24 @@ namespace Aditya_Dodiya_23SOECE11014
     {
         public static void Main(string[] args)
         {
-
+            int n = 8;
+            for(int i=1;i<=n; i++)
+            {
+                Console.Write(new string(' ', (n/2) - i)); 
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                    if(j < i)
+                    {
+                        Console.Write("*"); 
+                    }
+                }
+                Console.WriteLine();
+                if (i > 4)
+                {
+                    Console.Write(new string('*', (n / 2) - i));
+                }
+            }
         }
     }
 }
