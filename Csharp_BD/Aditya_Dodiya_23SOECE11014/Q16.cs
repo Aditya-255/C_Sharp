5b3598ebeaaa8579263255df0c94b8934fd725a1
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-07-02 18:49:55 +0530

    commit

---
+++ Csharp_BD/Aditya_Dodiya_23SOECE11014/Q16.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Aditya_Dodiya_23SOECE11014
+{
+    class Q16
+    {
+        public static void Main(string[] args)
+        {
+            int n = 3;
+            for(int i = 1; i <= n; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
