8138959e196398867976ebf47de36a8f442cd0a7
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-07-03 11:10:38 +0530

    Add file

---
+++ CSharp_PJT/Pattern5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//*
+//**
+//***
+//****
+//*****
+//****
+//***
+//**
+//*
+namespace CSharp_PJT
+{
+    internal class Pattern5
+    {
+        public static void Main(string[] args)
+        {
+            for(int i = 1; i <= 5; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for(int i = 5 - 1; i >= 1; i--)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
