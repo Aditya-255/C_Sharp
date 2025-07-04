ff235e6fc8838034ba9860e40cb715f005b70d66
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-07-04 13:27:55 +0530

    delete

---
+++ Csharp_BD/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_BD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //type casting 
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            //string interpolation 
+            Console.WriteLine($"sum of {a} and {b} is a "+(a + b));   
+        }
+    }
+}
