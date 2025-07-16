2d32afc15bac68682ed00b87d890cbec171a0198
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-07-16 11:10:53 +0530

    add tutorial 2

---
+++ Aditya_Dodiya_23SOECE11014/Tutorial-02/Q1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aditya_Dodiya_23SOECE11014.Tutorial_02
+{
+    internal class Q1
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr=new int[10];
+            arr[0] = 1;
+            arr[1] = 2;
+            arr[2] = 3;
+            arr[3] = 4;
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr[1]);
+            Console.WriteLine(arr[2]);
+            Console.WriteLine(arr[3]);
+        }
+    }
+}
