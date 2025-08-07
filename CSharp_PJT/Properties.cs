475d958c55b92b5b63f21738f686e04918493263
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-07 11:13:09 +0530

    add files

---
+++ CSharp_PJT/Properties.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_PJT
+{
+    class Student
+    {
+        private string name;
+        public string getname()
+        {
+            return name;
+        }
+        public void setname(string fname)
+        {
+            this.name=fname;
+        }
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+    internal class Properties
+    {
+        public static void Main()
+        {
+            Student obj = new Student();
+            obj.setname("ABC");
+            Console.WriteLine(obj.getname());
+
+            obj.Name = "ABC";
+            Console.WriteLine(obj.Name);
+
+        }
+    }
+}
---
+++ CSharp_PJT/T3_wex.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_PJT
+{
+    class Student3
+    {
+        private int enrno;
+        public int Enrno
+        {
+            get { return enrno;}
+            set { enrno = value; }
+        }
+        private string name;
+        public string Name
+        {
+            get{ return name;}
+            set { name = value; }
+        }
+        Student3()
+        {
+
+
+        }
+
+
+    }
+    internal class T3_wex
+    {
+        public static void Main()
+        {
+            Student3 s3=new Student3();
+            s3.Enrno = 1;
+            s3.Name = "Rushi";
+            
+            string t=s3.Name;
+            int t1 = s3.Enrno;
+            Console.WriteLine("Enrollment no.: " + t);
+            Console.WriteLine("Student name is : " + t1);
+        }
+    }
+}
