590accd5d1f39ff2d3438e0b38eeb55a1a529c8f
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-08 18:54:45 +0530

    registration design and files

---
+++ WindowsFormsApp/registration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class registration : Form
+    {
+        public registration()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"E:\\.NET FILE\\C_sharp\\WindowsFormsApp\\register_Database1.mdf\";Integrated Security=True");
+            con.Open();
+            string query = "";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Registerd");
+
+
+            con.Close();
+        }
+    }
+}
