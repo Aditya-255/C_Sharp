07b8b2f0a4ec93d10575a77a671f537ae361ac48
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-09-02 16:26:45 +0530

    create constructor problem

---
+++ CSharp_NVB/Ex.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_NVB
+{
+    public class Student15 {
+        int rno;
+        string name;
+        string branch;
+        static int sem = 5;
+        int s1, s2, s3;
+        double avg;
+        public Student15(int rno, string name, string branch, int s1, int s2, int s3)
+        {
+            this.rno = rno;
+            this.name = name;
+            this.branch = branch;
+            this.s1 = s1;
+            this.s2 = s2;
+            this.s3 = s3;
+        }
+
+        public void calcavg()
+        {
+            avg = (s1 + s2 + s3) / 3.0;
+        }
+        public double Avg
+        { 
+            get { return avg; }
+        }
+        public void showde()
+        {
+            Console.WriteLine("roll no: " + rno);
+            Console.WriteLine("name is :" + name);
+            Console.WriteLine("branch is: "+branch);
+            Console.WriteLine("semester is: " + sem);
+            Console.WriteLine("avg is : "+avg);
+        }
+    }
+
+    internal class Ex
+    {
+        public static void Main(string[] args)
+        {
+            Student15 s15 = new Student15(1, "Rushi" , "ce", 90, 85, 45); 
+            s15.calcavg();
+            Student15 s25 = new Student15(1, "krsih", "ceA", 91, 83, 44);
+            s25.calcavg();
+            if (s15.Avg > s25.Avg)
+            {
+                s15.showde();
+            }
+            else {
+                s25.showde();
+            }
+        }
+    }
+}
---
+++ CSharp_NVB/newQuw.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_NVB
+{
+    public class Booked {
+        int id;
+        string title;
+        string author;
+        int pages;
+        double oriprize;
+        int ch;
+
+        double dis1;
+        double dis2;
+        double dis3;
+    public Booked(int id, string title, string author, int pages, double oriprize, int ch)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.oriprize = oriprize;
+            this.ch = ch;
+        }
+
+        public void  Discount1()
+        {
+            dis1=oriprize-(oriprize*0.3);
+        }
+        public void Discount2()
+        {
+            dis2 = oriprize - (oriprize * 0.2);
+        }
+        public void Discount3()
+        {
+            dis3 = oriprize - (oriprize * 0.1);
+        }
+        public void showdetail()
+        {
+            Console.WriteLine("id is :" + id);
+            Console.WriteLine("book name is: " + title);
+            Console.WriteLine("author is : " + author);
+            Console.WriteLine("pages is : " + pages);
+            Console.WriteLine("price is : " + oriprize);
+            Console.WriteLine("chapter in this : " + ch);
+        }
+
+        public void disprices()
+        {
+            if (pages > 500)
+            {
+                Discount1();
+                Console.WriteLine(dis1);
+            }
+            else if(pages > 300)
+            {
+                Discount2();
+                Console.WriteLine(dis2);
+            }
+            else
+            {
+                Discount3();
+                Console.WriteLine(dis3);
+            }
+        }
+    }
+
+    internal class newQuw
+    {
+        public static void Main(String[] args)
+        {
+            Booked bb = new Booked(1, "maths", "abcd", 501, 100, 5);
+            bb.showdetail();
+
+            Console.WriteLine("discount prize is : "+bb.);
+        }
+    }
+}
