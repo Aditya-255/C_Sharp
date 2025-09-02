ea6acf955c297d2d1a060fa27e84d3037a3f7316
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-09-02 16:26:16 +0530

    create NVB Que

--- CSharp_NVB/Que_ex.cs
+++ CSharp_NVB/Que_ex.cs
@@ -12,9 +12,10 @@ public class Employee11
         int ecode;
         string name;
         string designation;
-        string instName;
         int basicpay;
-
+        double da;
+        double hra;
+        double totalpay;
         public int ECODE
         {
             get;
@@ -22,18 +23,39 @@ public class Employee11
         }
 
 
-        Employee11(int ecode,string name,string designation,string instName,int basicpay)
+        public Employee11(int ecode,string name,string designation,int basicpay)
         {
-
+            this.ecode = ecode;
+            this.name = name;
+            this.designation = designation;
+            this.basicpay = basicpay;
         }
-
+        public void calcDA()
+        {
+            da = 0.45 * basicpay;
+        }
+        public void calcHra()
+        {
+            hra = 0.2 * basicpay;
+        }
+        public void calctotal()
+        {
+            totalpay = basicpay + da + hra;
+        }
+        public void showdetails()
+        {
+            Console.WriteLine("total pay : "+totalpay);
+        }
     }
     internal class Que_ex
     {
         public static void Main(string[] args)
         {
-            Employee11 employee11 = new Employee11(1, "rushi", "HR", "RK", 10);
-
+            Employee11 e11 = new Employee11(1, "rushi", "HR", 10);
+            e11.calcDA();
+            e11.calcHra();
+            e11.calctotal();
+            e11.showdetails();
         }
     }
 }
