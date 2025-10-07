58dbd0cfcfcf23f4ebf9fa482e62e9a41b3645d6
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-10-07 15:59:46 +0530

    abstraction

---
+++ CSharp_NVB/Abs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_NVB
+{
+    abstract class X
+    {
+        protected int n1, n2;
+        public X(int n1, int n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+        public void add(int n1,int n2)
+        {
+            Console.WriteLine($"Addition: {n1 + n2}");
+        }
+        public abstract void mul(int n1, int n2);
+    }
+    internal class Abs
+    {
+        public static void Main(string[] args)
+        {
+            X obj = new Y(10, 20);
+            obj.add(10, 20);
+            obj.mul(10, 20);
+        }
+    }
+}
