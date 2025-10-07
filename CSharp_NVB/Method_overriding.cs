c964eb1e44ea818dfce599f5bbec0694c6e9641a
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-10-07 11:32:30 +0530

    method overriding

---
+++ CSharp_NVB/Method_overriding.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ * method overriding
+ * 
+ */
+namespace CSharp_NVB
+{
+    class P {
+        public virtual void show()
+        {
+            Console.WriteLine("parent class");
+        }
+    }
+    class C : P
+    {
+        public override void show()
+        {
+            P obj = new P();
+            base.show();
+            Console.WriteLine("child class");
+        }
+    }
+    internal class Method_overriding
+    {
+        public static void Main(string[] args)
+        {
+            P pobj;
+            C cobj = new C();
+            pobj = cobj;
+            pobj.show();
+            cobj.show();
+        }
+    }
+}
---
+++ Demo/Form3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Demo
+{
+    public partial class Form3 : Form
+    {
+        string connectionString = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Demo;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trusted_Connection = True";
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                // Simple query with LIKE
+                string query = "SELECT id, name, age FROM Students WHERE name LIKE @name";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                da.SelectCommand.Parameters.AddWithValue("@name", "%" + textBox1.Text + "%");
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dataGridView1.DataSource = dt;   // show result in DataGridView
+            }
+        }
+    }
+}
